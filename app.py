b9001e3636d81dacc584586079faed12c77540f3
jocrz <jojo3416jo@gmail.com>
2024-02-21 13:37:07 +0530

    updated

--- app.py
+++ app.py
@@ -12,20 +12,22 @@ PAGE_TITLE = "Digital CV | JOJO VARGHESE"
 PAGE_ICON = ":wave:"
 NAME = "JOJO VARGHESE"
 DESCRIPTION = """
-Junior Data Scientist, looking forward to support enterprises for data-driven decision-making.
+Junior Data Scientist, looking forward to apply analytical skills to real-world problems and learn through
+the experience.
+Curious Data Scientist eager to 
 """
 EMAIL = "jojo3416jo@gmail.com"
 SOCIAL_MEDIA = {
-    "YouTube": "https://youtube.com/c/codingisfun",
     "LinkedIn": "https://www.linkedin.com/in/jojo-varghese-datastronaut/",
     "GitHub": "https://github.com/JOCRZ",
-    "Twitter": "https://twitter.com",
+    "Medium": "https://medium.com/@jojo3416jo",
+    "Hackerrank": "https://www.hackerrank.com/profile/jojo3416jo",
 }
 PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
-    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
+    "🏆 Employee Turn Over Prediction - Logistic Regression": "https://employechurnprediction.streamlit.app/",
+    "🏆 Recruit Salary Prediction - Linear Regression": "https://recruitsalarypredictor.streamlit.app/",
+    "🏆 Iris Flower Classification - Decision Tree": "https://irisclassifications.streamlit.app/",
+    "🏆 Walmart Sales Dashboard - LookerStudio": "https://lookerstudio.google.com/reporting/8bd21894-3814-44ac-83b5-ff2d7a454b2e",
 }
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
@@ -53,29 +55,26 @@ cols = st.columns(len(SOCIAL_MEDIA))
 for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
     cols[index].write(f"[{platform}]({link})")
 
-# --- EXPERIENCE & QUALIFICATIONS ---
-st.write('\n')
-st.subheader("Experience & Qualifications")
-st.write(
-    """
-- ✔️ 7 Years experience extracting actionable insights from data
-- ✔️ Strong hands-on experience and knowledge in Python and Excel
-- ✔️ Good understanding of statistical principles and their respective applications
-- ✔️ Excellent team-player and displaying a strong sense of initiative on tasks
-"""
-)
+
 
 # --- SKILLS ---
 st.write('\n')
 st.subheader("Hard Skills")
 st.write(
     """
-- 👩‍💻 Programming: Python (Scikit-learn, Pandas), SQL, VBA
-- 📊 Data Visualization: PowerBi, MS Excel, Plotly
-- 📚 Modeling: Logistic regression, linear regression, decision trees
-- 🗄️ Databases: Postgres, MongoDB, MySQL
+-  Programming: Python, SQL
+-  Data Science : Pandas, Numpy, Scikit-learn, Keras, Seaborn, Matplotlib
+-  Databases: Postgres, MySQL
+-  Frame work : Streamlit, Flask
+-  WebScrapping : BeautifulSoup
+-  Dashboarding : PowerBi, MS Excel, LookerStudio
+-  Algorithms : Linear regression, Logistic regression, Decision trees, Random Forest, SVM, KNN
+-  Clustering : K-Means
+-  Neural Networks : ANN, CNN, RNN
+-  Version Control : Git, Git-Hub
 """
 )
+
 
 # --- WORK HISTORY ---
 st.write('\n')
@@ -83,43 +82,71 @@ st.subheader("Work History")
 st.write("---")
 
 # --- JOB 1 ---
-st.write("🚧", "**Senior Data Analyst | Ross Industries**")
-st.write("02/2020 - Present")
+st.write("**Data Scientist Intern | The Sparks Foundation**")
+st.write("December 2023 - January 2024")
 st.write(
     """
-- ► Used PowerBI and SQL to redefine and track KPIs surrounding marketing initiatives, and supplied recommendations to boost landing page conversion rate by 38%
-- ► Led a team of 4 analysts to brainstorm potential marketing and sales improvements, and implemented A/B tests to generate 15% more client leads
-- ► Redesigned data model through iterations that improved predictions by 12%
+- ► The Internship involved completing multiple tasks, including improving my LinkedIn prole,
+peer evaluation, and creating a credible resume on mycredible. The project primarily
+centered around using a Decision Tree to classify the iris flower. By focusing solely on this
+algorithm, I gained in-depth knowledge about its workings.
 """
 )
 
 # --- JOB 2 ---
 st.write('\n')
-st.write("🚧", "**Data Analyst | Liberty Mutual Insurance**")
-st.write("01/2018 - 02/2022")
+st.write("**Data Analyst Intern | Meriskill**")
+st.write("October 2023 - Novermber 2023")
 st.write(
     """
-- ► Built data models and maps to generate meaningful insights from customer data, boosting successful sales efforts by 12%
-- ► Modeled targets likely to renew, and presented analysis to leadership, which led to a YoY revenue increase of $300K
-- ► Compiled, studied, and inferred large amounts of data, modeling information to drive auto policy pricing
+- ► During the Internship, I completed three projects:
+- ► Sales Data Analysis: Explored a large sales dataset, identifying trends, top-selling products,
+and revenue metrics. Applied feature engineering to enhance the dataset and created clear
+visualizations to convey fndings.
+- ► Gestational Diabetes Prediction: Cleaned and prepared data, addressed data imbalance,
+and built a predictive model with 77% accuracy. Deployed the model on Streamlit for
+accessibility.
+- ► HR Attrition Analysis: Conducted EDA, cleaned data, and utilized statistical techniques to
+uncover insights into HR dynamics. Developed a dashboard to efectively communicate
+fndings to stakeholders.
 """
 )
 
 # --- JOB 3 ---
 st.write('\n')
-st.write("🚧", "**Data Analyst | Chegg**")
-st.write("04/2015 - 01/2018")
+st.write("**Data Scientist | British Airways**")
+st.write("8 October 2023")
 st.write(
     """
-- ► Devised KPIs using SQL across company website in collaboration with cross-functional teams to achieve a 120% jump in organic traffic
-- ► Analyzed, documented, and reported user survey results to improve customer communication processes by 18%
-- ► Collaborated with an analyst team to oversee the end-to-end process surrounding customers' return data
+- ► Web Scraping and EDA: Task 1 web scraping on British Airways customer reviews using Pandas and BeautifulSoup.
+Performed Exploratory Data Analysis (EDA) on the scraped data to extract insights and identify trends.
+- ► Data Cleaning and Wrangling: Cleaned and wrangled the scraped data to prepare it for analysis and modeling.
+- ► Predictive Modeling: Task 2 Built a predictive model using statistical methods for feature selection and engineering.
+- ► Experimented with various Machine Learning (ML) algorithms to find the best-performing one.
+- ► Model Evaluation: Evaluated the predictive model's performance, achieving a high accuracy of 0.99.
+Assessed the model's precision, indicating a low False Positive Rate of 0.074%.
+"""
+)
+# --- JOB 4 ---
+st.write('\n')
+st.write("**Data Analyst | Accenture**")
+st.write("7 April 2023")
+st.write(
+    """
+- ►Understanding Project Requirements: Collaborate with project teams to comprehend scope, objectives, and deliverables of data analysis projects.
+Gain a thorough understanding of client requirements, business processes, and data sources. 
+- ►  Data Cleaning and Modeling: Apply data cleaning techniques to ensure data accuracy, consistency, and completeness.
+- ► Data Analysis and Insights: Perform in-depth data analysis to identify trends, patterns, and correlations.
+- ►  Visualization and Storytelling: Create visually appealing and informative data visualizations using Matplotlib.
+Develop compelling narratives to communicate insights effectively using tools like PowerPoint.
+- ► Client Presentation: Prepare and deliver presentations to clients, effectively conveying insights derived from data analysis.
+
 """
 )
 
 # --- PROJECTS & ACCOMPLISHMENTS ---
 st.write('\n')
-st.subheader("Projects & Accomplishments")
+st.subheader("Projects")
 st.write("---")
 for project, link in PROJECTS.items():
     st.write(f"[{project}]({link})")
